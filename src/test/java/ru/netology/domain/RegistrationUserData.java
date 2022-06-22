c864b239c93aa6acf1658774e82376649b49e051
DemKosS <sidyakinkn@gmail.com>
2022-06-22 23:21:56 +0300

    init

---
+++ src/test/java/ru/netology/domain/RegistrationUserData.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+
+
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class RegistrationUserData {
+    private final String login;
+    private final String password;
+    private final String status;
+}
